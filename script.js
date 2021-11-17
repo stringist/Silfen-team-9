8c9d00a558a5a0805cd5e82a9edc5fdb3145f8bd
Kalle Tiihonen <kall1429@stud.kea.dk>
2021-11-17 14:46:04 +0100

    copy pasted JS from previous project

--- script.js
+++ script.js
@@ -7,8 +7,63 @@ function closeMenu() {
 
 
 
-    }
-    
-    closeMenu();
- 
+}
 
+closeMenu();
+
+// fetching from database
+// fetching from database 
+// fetching from database
+// fetching from database
+
+const urlParams = new URLSearchParams(window.location.search);
+const bikename = urlParams.get("title.rendered");
+const endpoint = "https://lumensity.dk/recreate/wp-json/wp/v2/bike?_embed"
+
+
+fetch(endpoint)
+
+.then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        handleProductList(data);
+    });
+
+function handleProductList(data) {
+    console.log(data);
+    data.forEach(showData);
+
+    function makeColorBox(colorList) {
+        console.log(colorList);
+        colorList.forEach()
+    }
+}
+
+function showData(oneBike) {
+    const template = document.querySelector("#bikeTemplate").content;
+    const copy = template.cloneNode(true);
+    // const colors = oneBike.colors;
+    // const colorList = colors.split(",");
+    copy.querySelector("h3.brand").textContent = oneBike.brand;
+    copy.querySelector("h3.name").textContent = oneBike.title.rendered;
+    // copy.querySelector("img").setAttribute("src", oneBike._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url);
+    copy.querySelector("img").setAttribute("src", oneBike._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url);
+    // copy.querySelector("img").src = " oneBike._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url";
+    copy.querySelector(".price").textContent = oneBike.price;
+    // copy.querySelector(".color").textContent = oneBike.colors;
+    //copy.querySelector(".stock").textContent = oneBike.in_stock;
+
+    const colors = oneBike.colors.split(',')
+    colors.forEach(color => {
+        const li = document.createElement('li')
+            // li.textContent = color;
+        li.style.background = color
+        copy.querySelector('.colorbox ul').appendChild(li)
+    })
+
+    console.log(oneBike.in_stock);
+    if (oneBike.in_stock == '1') { copy.querySelector(".stock").textContent = "yes" }
+    const parent = document.querySelector("main");
+    parent.appendChild(copy);
+}
