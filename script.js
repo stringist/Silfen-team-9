c89d39866434e12807463f607322a7e133996e76
PAUL_BALTA <welfryed73@gmail.com>
2021-11-17 02:09:24 +0100

    this is the category menu

---
+++ script.js
@@ -0,0 +1,14 @@
+function closeMenu() {
+
+    document.querySelector("#sign").classList.add("plus_sign");
+    document.querySelector("#sign_v2").classList.add("plus_sign");
+    document.querySelector("#sign_v3").classList.add("plus_sign");
+    document.querySelector("#sign_v4").classList.add("plus_sign");
+
+
+
+    }
+    
+    closeMenu();
+ 
+
